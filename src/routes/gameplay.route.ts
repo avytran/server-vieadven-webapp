b584da2d4efe1089c83d4431df3cd35a2c283007
giahuytr <huyhaibabon861@gmail.com>
2025-04-27 02:32:25 +0700

    Feature/Buld API To Get Gameplays By Province ID And User ID (#22)

--- src/routes/gameplay.route.ts
+++ src/routes/gameplay.route.ts
@@ -5,5 +5,6 @@ import gameplayController from '../controllers/gameplay.controller';
 const router = express.Router();
 
 router.post('/update', gameplayController.updateGameplay);
+router.get('/:province_id/user/:user_id', gameplayController.getGameplaysByUserIdAndProvinceId)
 
 export default router;
