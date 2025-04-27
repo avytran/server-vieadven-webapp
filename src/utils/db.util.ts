a31ad1fc8652a17850a3ab4f4909c99fa2c18e4e
giahuytr <huyhaibabon861@gmail.com>
2025-04-27 19:13:22 +0700

    Fix/Update DB Util (#23)

--- src/utils/db.util.ts
+++ src/utils/db.util.ts
@@ -4,15 +4,23 @@ const config: { [key: string]: Knex.Config } = {
     development: {
         client: 'pg',
         connection: {
-            host: 'localhost',
-            user: 'vieadvendev',
-            password: 'vieadvendev',
-            database: 'vieadvenweb',
-            port: 5433,
+            host: 'aws-0-ap-southeast-1.pooler.supabase.com',  // Thay bằng host của Supabase hoặc PgBouncer
+            user: 'postgres.oycqdxhlckxpebthkdhc',               // Username Supabase
+            password: 'etclubweb',   // Mật khẩu của bạn
+            database: 'postgres',           // Tên database (ví dụ 'postgres')
+            port: 5432,                     // Cổng kết nối PostgreSQL
+            ssl: { rejectUnauthorized: false } // Bật SSL cho Supabase
+        },
+        pool: {
+            min: 2,     // Số kết nối tối thiểu trong pool
+            max: 10,    // Số kết nối tối đa trong pool
+            acquireTimeoutMillis: 30000,  // Thời gian tối đa để chờ kết nối từ pool
+            createTimeoutMillis: 3000,    // Thời gian tối đa để tạo kết nối mới
+            idleTimeoutMillis: 10000,     // Thời gian tối đa một kết nối không được sử dụng sẽ bị giải phóng
         }
     }
 };
 
 const db = knex(config['development']);
 
-export default db; 
+export default db;
